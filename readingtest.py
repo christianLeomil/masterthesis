b40fd430a6624e115a6b2bc3447269f2d7c55698
christianLeomil <christianleomil@gmail.com>
2023-05-18 15:41:21 +0200

    Deactivating constraints

--- readingtest.py
+++ readingtest.py
@@ -1,4 +1,7 @@
 from pyomo.environ import *
+import pandas as pd
+
+path_output = './masterthesis/'
 
 model = AbstractModel()
 
@@ -7,19 +10,26 @@ model.HOURS = Set()
 
 # Parameters
 model.demand = Param(model.HOURS)
-model.costA = Param(initialize = 20)
-model.costB = Param(initialize = 15)
+model.costA = Param(initialize=20)
+model.costB = Param(initialize=15)
 model.workCapA = Param(model.HOURS)
 model.workCapB = Param(model.HOURS)
 
 # Variables
-model.amountA = Var(model.HOURS, within=NonNegativeIntegers)
+model.amountA = Var(model.HOURS, within=NonNegativeIntegers, initialize=10)
 model.amountB = Var(model.HOURS, within=NonNegativeIntegers)
 
 # Constraints
 def demand_rule(model, t):
     return model.amountA[t] * model.workCapA[t] + model.amountB[t] * model.workCapB[t] >= model.demand[t]
-model.demand_rule = Constraint(model.HOURS, rule=demand_rule)
+model.demandRule = Constraint(model.HOURS, rule=demand_rule)
+
+def time_step_rule(model, t):
+    if t == 1:
+        return model.amountA[t] == 10
+    else: 
+        return model.amountA[t] <= 1.5 * model.amountA[t-1]
+model.timeStepRule = Constraint(model.HOURS, rule=time_step_rule)
 
 # Objective function
 def objective_rule(model):
@@ -29,11 +39,13 @@ model.objective_function = Objective(rule=objective_rule, sense=minimize)
 # Reading data
 data = DataPortal()
 data.load(filename='HOURS.csv', set='HOURS')
-data.load(filename='demand.csv', param='demand',index = 'HOURS')
-data.load(filename='workCapA.csv', param='workCapA',index = 'HOURS')
-data.load(filename='workCapB.csv', param='workCapB',index = 'HOURS')
+data.load(filename='demand.csv', param='demand', index='HOURS')
+data.load(filename='workCapA.csv', param='workCapA', index='HOURS')
+data.load(filename='workCapB.csv', param='workCapB', index='HOURS')
 
 instance = model.create_instance(data)
+
+# instance.timeStepRule.deactivate()
 
 # Solving the model
 optimizer = SolverFactory('cplex')
@@ -42,3 +54,18 @@ results = optimizer.solve(instance)
 # Displaying the results
 instance.pprint()
 instance.display()
+
+# Exporting variable values to Excel
+variable_values = {
+    'Hours': [],
+    'AmountA': [],
+    'AmountB': []
+}
+
+for t in instance.HOURS:
+    variable_values['Hours'].append(str(t))
+    variable_values['AmountA'].append(instance.amountA[t].value)
+    variable_values['AmountB'].append(instance.amountB[t].value)
+
+df = pd.DataFrame(variable_values)
+df.to_excel(path_output + 'variable_values.xlsx', index=False)
