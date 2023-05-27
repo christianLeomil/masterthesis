1a2770b96baf23457286a3d19001e63e14a381b6
christianLeomil <christianleomil@gmail.com>
2023-05-27 09:57:02 -0300

    dynamically creating expressions

--- test.py
+++ test.py
@@ -17,30 +17,45 @@
 # dynamic_obj.method_1(1)
 # dynamic_obj.method_2(2)
 
-import pandas as pd
+# import pandas as pd
 
-path = './input/'
-name_file = 'c_matrix.xlsx'
+# path = './input/'
+# name_file = 'c_matrix.xlsx'
 
-df = pd.read_excel(path + name_file)
-df.set_index('from', inplace=True)
-df.index.name = None
+# df = pd.read_excel(path + name_file)
+# df.set_index('from', inplace=True)
+# df.index.name = None
 
-class dynamic_class:
-    def __init__(self,df):
-        self.df = df
+# class dynamic_class:
+#     def __init__(self,df):
+#         self.df = df
 
-        for i in range(0,len(df)):
-            for j in df.columns:
-                if df[j].iloc[i] == 1:
-                    name = str(df.index[i]) + '_' + str(j)
-                    print(name)
-                    def dynamic_method(self,num):
-                        return print(num*10)       
-                    setattr(self,name,dynamic_method.__get__(self, dynamic_class))
+#         for i in range(0,len(df)):
+#             for j in df.columns:
+#                 if df[j].iloc[i] == 1:
+#                     name = str(df.index[i]) + '_' + str(j)
+#                     print(name)
+#                     def dynamic_method(self,num):
+#                         return print(num*10)       
+#                     setattr(self,name,dynamic_method.__get__(self, dynamic_class))
 
-dynamic_obj = dynamic_class(df)
-dynamic_obj.bat_demand(4)
+# dynamic_obj = dynamic_class(df)
+# dynamic_obj.bat_demand(4)
 
-method_list = [method for method in dir(dynamic_obj) if method.startswith('__') is False]
+# method_list = [method for method in dir(dynamic_obj) if method.startswith('__') is False]
+# print(method_list)
+
+
+# print(eval("2 ** 8"))
+# eval("pi = 3.1416")
+
+vector = ['pv','pv_bat','pv_demand']
+
+class teste1:
+    def teste(vector):
+        globals()[vector[0]] = globals()[vector[1]] +globals()[vector[2]]
+
+teste2 = teste1
+
+method_list = [method for method in dir(teste2) if method.startswith('__') is False]
 print(method_list)
