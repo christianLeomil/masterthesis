0b8a9694705c88b8c5905ebdccb51da869091be3
christianLeomil <christianleomil@gmail.com>
2023-05-27 11:18:08 -0300

    further evolution

--- test.py
+++ test.py
@@ -1,3 +1,48 @@
+# from pyomo.environ import *
+
+# class ExampleClass:
+#     def __init__(self, param1, param2):
+#         self.param1 = param1
+#         self.param2 = param2
+
+#     def func1(self, x):
+#         return self.param1 * x
+
+#     def func2(self, x, y):
+#         return self.param2 * x + y
+
+# class AbstractModel:
+#     def __init__(self, example):
+#         self.example = example
+#         self.model = AbstractModel()
+
+#         self.model.x = Var(domain=NonNegativeReals)
+#         self.model.y = Var(domain=NonNegativeReals)
+#         self.model.obj = Objective(rule=self.objective_rule)
+
+#         self.model.constraints = ConstraintList()
+#         self.model.constraints.add(self.example.func1(self.model.x) >= 10)
+#         self.model.constraints.add(self.example.func2(self.model.x, self.model.y) <= 20)
+
+#     def objective_rule(self, model):
+#         return self.example.func1(model.x) + self.example.func2(model.x, model.y)
+
+#     def solve(self):
+#         solver = SolverFactory('glpk')
+#         instance = self.model.create_instance()
+#         results = solver.solve(instance)
+
+#         if results.solver.termination_condition == TerminationCondition.optimal:
+#             instance.solutions.load_from(results)
+#             instance.display()
+
+# example = ExampleClass(2, 3)
+# abstract_model = AbstractModel(example)
+# abstract_model.solve()
+
+
+
+
 # class DynamicClass:
 #     def __init__(self, n):
 #         self.n = n
@@ -49,13 +94,42 @@
 # print(eval("2 ** 8"))
 # eval("pi = 3.1416")
 
-vector = ['pv','pv_bat','pv_demand']
+# vector = ['pv','pv_bat','pv_demand']
 
-class teste1:
-    def teste(vector):
-        globals()[vector[0]] = globals()[vector[1]] +globals()[vector[2]]
+# class teste1:
+#     def teste(vector):
+#         globals()[vector[0]] = globals()[vector[1]] +globals()[vector[2]]
 
-teste2 = teste1
+# teste2 = teste1
 
-method_list = [method for method in dir(teste2) if method.startswith('__') is False]
-print(method_list)
+# method_list = [method for method in dir(teste2) if method.startswith('__') is False]
+# print(method_list)
+
+
+#-------------------------------------------------------------------------------------------
+
+from pyomo.environ import *
+
+model = ConcreteModel()
+
+# Define variables
+model.x = Var(domain=NonNegativeReals)
+model.y = Var(domain=NonNegativeReals)
+
+# Define a parameter
+param = 5
+
+# Define a dynamic expression
+model.dynamic_expr = Expression(expr=model.x + model.y <= param)
+
+# Define a constraint using the dynamic expression
+model.constraint = Constraint(expr=model.dynamic_expr)
+
+# Solve the model
+solver = SolverFactory('glpk')
+results = solver.solve(model)
+
+# Print the results
+print("x =", model.x.value)
+print("y =", model.y.value)
+
