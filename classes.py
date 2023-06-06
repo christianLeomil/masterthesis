44e933f9a54d8bf0703b5410f1168aff2ed597cc
christianLeomil <christianleomil@gmail.com>
2023-06-06 10:08:35 +0200

    Looping through constraints of class

--- classes.py
+++ classes.py
@@ -52,8 +52,8 @@ class Subclass(Testing):
     def __init__(self,model,t):
         super().__init__(model,t)
 
-    def second_rule(model,t):
-        return model.quant_x[t] == model.quant_z[t]
+    # def second_rule(model,t):
+    #     return model.quant_x[t] == model.quant_z[t]
          
 
      
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pyomo.environ as pyo
-# from classes import Subclass
+from classes import Subclass
+import inspect
 
 path_output = './output/'
 path_input = './input/'
@@ -32,26 +33,32 @@ model.quant_z = pyo.Var(model.HOURS, within=pyo.NonNegativeReals)
 #     def second_rule(model,t):
 #         return model.quant_x[t] == model.quant_z[t]
 
-class Testing:
-    def demand_rule(model,t):
-        return model.demand[t] == model.quant_x[t] + model.quant_y[t]
+# class Testing:
+#     def demand_rule(model,t):
+#         return model.demand[t] == model.quant_x[t] + model.quant_y[t]
 
-class Subclass(Testing):
-    def __init__(self,model,t):
-        super().__init__(model,t)
+# class Subclass(Testing):
+#     def __init__(self,model,t):
+#         super().__init__(model,t)
 
-    def second_rule(model,t):
-        return model.quant_x[t] == model.quant_z[t]
+#     def second_rule(model,t):
+#         return model.quant_x[t] == model.quant_z[t]
 
 subclass_testing = Subclass
 
-model.demandRule = pyo.Constraint(model.HOURS,rule = subclass_testing.demand_rule)
+constraint_number = 1
+for name,method in inspect.getmembers(subclass_testing,predicate=inspect.isfunction):
+    if name != '__init__':
+        setattr(model,'constraint_'+str(constraint_number),pyo.Constraint(model.HOURS,rule = method))
+    constraint_number += 1
+
+# model.demandRule = pyo.Constraint(model.HOURS,rule = subclass_testing.demand_rule)
 # model.secondRule = pyo.Constraint(model.HOURS, rule = subclass_testing.second_rule)
 
 #objective
 def objective_rule(model, t):
     return sum(model.cost_x[t] * model.quant_x[t] + model.cost_y[t] * model.quant_y[t] + model.cost_x[t] *
-               model.quant_z[t] for t in model.HOURS)
+            model.quant_z[t] for t in model.HOURS)
 
 model.objectiveRule = pyo.Objective(rule=objective_rule, sense=pyo.minimize)
 
