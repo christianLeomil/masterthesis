497cfc92403e4c3ad5f90f886a3deb64938fdc26
christianLeomil <christianleomil@gmail.com>
2023-06-15 20:44:06 +0200

    Delete hyperclass.py

--- hyperclass.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def define_hyper_class(path_input,path_output):
-    df_hyper = pd.read_excel(path_input + 'df_input.xlsx',sheet_name='hyperclass')
-    return df_hyper
-
-
-
-
