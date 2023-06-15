5f40f774ff63a48d590940bbc074c7d408d830bf
christianLeomil <christianleomil@gmail.com>
2023-06-15 23:14:56 +0200

    primary alterations

--- functions.py
+++ functions.py
@@ -1,4 +1,34 @@
 import pandas as pd
+from openpyxl import load_workbook
+
+def matrix_creator(df_elements, path_input, name_file):
+    list_elements = []
+    for i in df_elements.columns:
+        for j in range(0, df_elements[i].iloc[0]):
+            list_elements.append(i + str(j+1))
+
+    df_matrix = pd.DataFrame(0, index=list_elements, columns=list_elements)
+    print(df_matrix)
+
+    book = load_workbook(path_input + name_file)
+    writer = pd.ExcelWriter(path_input + name_file, engine='openpyxl') 
+    writer.book = book
+
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+    df_matrix.to_excel(writer, "Main")
+
+    writer.save()
+
+    return df_matrix
+
+path_input = './input/'
+name_file = 'df_input.xlsx'
+
+df_elements = pd.read_excel(path_input + name_file, sheet_name='elements')
+df_matrix = matrix_creator(df_elements, path_input, name_file)
+
+print(df_matrix)
 
 def connection_creator(df_conect,df_aux,path_output):
 
@@ -95,12 +125,16 @@ def exp_creator(df_conect,df_variables):
 
     return list_expressions
 
-# path_output = './output/'
 # path_input = './input/'
 # name_file = 'df_input.xlsx'
 # df_conect = pd.read_excel(path_input + name_file,sheet_name='conect',index_col=0)
 # df_conect.index.name = None
 # df_aux = pd.read_excel(path_input + name_file,sheet_name='aux')
+
+# df_elements = pd.read_excel(path_input + name_file, sheet_name = 'elements')
+# df_matrix = matrix_creator(df_elements, path_input, name_file)
+
+# print(df_matrix)
 
 # [df_conect, df_variables] = connection_creator(df_conect,df_aux,path_output)
 # list_expressions = exp_creator(df_conect,df_variables)
