540b8026caa37b91c33138b359ee24f0c315a31d
christianLeomil <christianleomil@gmail.com>
2023-06-16 12:36:40 +0200

    whole method
    tried with the whole method

--- functions.py
+++ functions.py
@@ -8,134 +8,23 @@ def matrix_creator(df_elements, path_input, name_file):
             list_elements.append(i + str(j+1))
 
     df_matrix = pd.DataFrame(0, index=list_elements, columns=list_elements)
-    print(df_matrix)
-
-    book = load_workbook(path_input + name_file)
-    writer = pd.ExcelWriter(path_input + name_file, engine='openpyxl') 
-    writer.book = book
-
-    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-
-    df_matrix.to_excel(writer, "Main")
-
-    writer.save()
 
     return df_matrix
 
-path_input = './input/'
-name_file = 'df_input.xlsx'
+def connection_creator(df_conect,path_output):
+    df_conect
 
-df_elements = pd.read_excel(path_input + name_file, sheet_name='elements')
-df_matrix = matrix_creator(df_elements, path_input, name_file)
-
-print(df_matrix)
-
-def connection_creator(df_conect,df_aux,path_output):
-
-    list_variables = []
-    list_variables_aux = []
-
-    for i in df_conect.index:
-        for j in df_conect.columns:
-            if df_conect.loc[i,j] != 0:
-                list_variables.append('P_to_' + i)
-                if i not in ['demand','net']:
-                    list_variables_aux.append('t,' + df_aux[i].iloc[0])
-                else:
-                    list_variables_aux.append('t')
-                break
-
-    for i in df_conect.columns:
-        for j in df_conect.index:
-            if df_conect.loc[j,i] != 0:
-                list_variables.append('P_from_' + i)
-                if i not in ['demand','net']:
-                    list_variables_aux.append('t,' + df_aux[i].iloc[0])
-                else:
-                    list_variables_aux.append('t')   
-                break
-
-    for i in df_conect.index:
-        for j in df_conect.columns:
-            if df_conect.loc[i,j] != 0:
-                list_variables.append('P_' + j +  '_' + i)
-                if (i not in ['demand','net']) and (j not in ['demand','net']):
-                    list_variables_aux.append('t,'+df_aux[j].iloc[0]+','+df_aux[i].iloc[0])
-                elif (i in ['demand','net']) and (j in ['demand','net']):
-                    list_variables_aux.append('t')
-                else:
-                    if  i in ['demand','net']:
-                        list_variables_aux.append('t,'+ df_aux[j].iloc[0])
-                    else:
-                        list_variables_aux.append('t,'+ df_aux[i].iloc[0])
-
-    df_variables = pd.DataFrame({'variables': list_variables,
-                                'aux':list_variables_aux})
-
-    for i in df_conect.index:
-        for j in df_conect.columns:
-            if df_conect.loc[i,j] == 1:
-                df_conect.loc[i,j] = 'P_' + j + '_' + i
-
-    df_conect.columns  = ['P_from_' + i for i in df_conect.columns]
-    df_conect.index = ['P_to_' + i for i in df_conect.index]
-
-    df_variables.to_excel(path_output + 'df_variables.xlsx')
-    df_conect.to_excel(path_output + 'df_conect.xlsx')
-    
     return df_conect, df_variables
 
-def exp_creator(df_conect,df_variables):
+path_input = './input/'
+path_output = './output/' 
+name_file = 'df_input.xlsx'
 
-    list_expressions = []
-    
-    #looping through columns, building "energy to" expressions
-    for i in df_conect.index:
-        list_exp_partial = []
-        for j,m in enumerate(df_conect.columns):
-            if df_conect.loc[i,m] != 0:
-                if list_exp_partial == []:
-                    suffix = df_variables[df_variables['variables'] == i]['aux'].iloc[0]
-                    list_exp_partial.append('model.' + i + '['+ suffix + ']' + ' == ')
-                    suffix = df_variables[df_variables['variables'] == df_conect.loc[i,m]]['aux'].iloc[0]
-                    list_exp_partial[-1] = list_exp_partial[-1] +  'model.'+df_conect.loc[i,m] + '['+ suffix + ']'
-                else:
-                    suffix = df_variables[df_variables['variables'] == df_conect.loc[i,m]]['aux'].iloc[0]
-                    list_exp_partial[-1] = list_exp_partial[-1] + ' + model.' + df_conect.loc[i,m] + '['+ suffix + ']'
-        if list_exp_partial != []:
-            list_exp_partial = list_exp_partial[-1] 
-            list_expressions.append(list_exp_partial)
-
-    #looping through columns, building "energy from" expressions
-    for i in df_conect.columns:
-        list_exp_partial = []
-        for j,m in enumerate(df_conect.index):
-            if df_conect.loc[m,i] != 0:
-                if list_exp_partial == []:
-                    suffix = df_variables[df_variables['variables'] == i]['aux'].iloc[0]
-                    list_exp_partial.append('model.' + i + '['+ suffix + ']' + ' == ')
-                    suffix = df_variables[df_variables['variables'] == df_conect.loc[m,i]]['aux'].iloc[0]
-                    list_exp_partial[-1] = list_exp_partial[-1] + 'model.' + df_conect.loc[m,i] + '['+ suffix + ']'
-                else:
-                    suffix = df_variables[df_variables['variables'] == df_conect.loc[m,i]]['aux'].iloc[0]
-                    list_exp_partial[-1] = list_exp_partial[-1] + ' + model.' + df_conect.loc[m,i] + '['+ suffix + ']'
-        if list_exp_partial != []:
-            list_exp_partial = list_exp_partial[-1] 
-            list_expressions.append(list_exp_partial)
-
-    return list_expressions
-
-# path_input = './input/'
-# name_file = 'df_input.xlsx'
-# df_conect = pd.read_excel(path_input + name_file,sheet_name='conect',index_col=0)
-# df_conect.index.name = None
-# df_aux = pd.read_excel(path_input + name_file,sheet_name='aux')
-
+# first run this
 # df_elements = pd.read_excel(path_input + name_file, sheet_name = 'elements')
 # df_matrix = matrix_creator(df_elements, path_input, name_file)
+# df_matrix.to_excel(path_output + 'df_matrix.xlsx')
 
-# print(df_matrix)
-
-# [df_conect, df_variables] = connection_creator(df_conect,df_aux,path_output)
-# list_expressions = exp_creator(df_conect,df_variables)
-# print(list_expressions)
+# than copy and paste df_matrix to sheet 'conect' and run this
+df_conect = pd.read_excel(path_input + name_file,sheet_name='conect',index_col=0)
+df_conect.index.name = None
--- test.py
+++ test.py
@@ -1,140 +1,101 @@
-# import pandas as pd
-# import pyomo.environ as pyo
-
-# #model
-# model = pyo.AbstractModel()
-
-# #sets
-# model.HOURS = pyo.Set()
-# model.PV = pyo.Set()
-# model.BAT = pyo.Set()
-
-# #varables
-# model.P_pv_demand = pyo.Var(model.HOURS, model.PV,within = pyo.NonNegativeReals)
-# model.P_bat_demand = pyo.Var(model.HOURS, model.BAT,within = pyo.NonNegativeReals)
-# model.P_net_demand = pyo.Var(model.HOURS, within = pyo.NonNegativeReals)
-
-# name_file = 'df_input.xlsx'
-# path_output = './output/'
-# path_input = './input/'
-
-# df_conect_to  = pd.read_excel(path_input + name_file, sheet_name = 'connect_to',index_col=0)
-# df_conect_to.index.name = None
-
-# list_energy_from = []
-# list_energy_to_total = []
-# for i in range(0, len(df_conect_to)):
-#     list_energy_to = []
-#     for j in df_conect_to.columns:
-#         if df_conect_to[j].iloc[i] == 1:
-#             list_energy_to.append(j)
-#     if list_energy_to != []:
-#         list_energy_to_total.append(list_energy_to)
-#         list_energy_from.append(df_conect_to.index[i])
-
-# df_conect_to = pd.DataFrame({'from': list_energy_from, 'to': list_energy_to_total})
-# print(df_conect_to)
-# print('\n')
-
-# df_sets = pd.DataFrame({'name':['pv','bat'],
-#                           'var':['n','m'],
-#                           'set':['model.PV','model.BAT']})
-
-# list_expressions = []
-# list_sets = []
-# for i in df_conect_to.index:
-#     list_expression_conect =[]
-#     if df_conect_to['from'].iloc[i] in ['demand','net']:
-#         list_expression_conect.append('model.P_to_' + df_conect_to['from'].iloc[i] + '[t] == ')
-#         list_sets.append('')
-#     else:
-#         list_expression_conect.append('model.P_to_' + df_conect_to['from'].iloc[i] + '[t][' + df_sets['var'][df_sets['name'] == df_conect_to['from'].iloc[i]].iloc[0] + '] == ')
-#         list_sets.append(df_sets['set'][df_sets['name'] == df_conect_to['from'].iloc[i]].iloc[0])
-#     for index,j in enumerate(df_conect_to['to'].iloc[i]):
-#         if index != 0:
-#             list_expression_conect[-1] = list_expression_conect[-1] + ' +'
-#         if j not in ['demand','net']:
-#             list_expression_conect[-1] = list_expression_conect[-1] + ' sum(model.P_' + j + '_' + df_conect_to['from'].iloc[i] +'[t][' + str(df_sets['var'][df_sets['name']==j].iloc[0]) +'] for ' + str(df_sets['var'][df_sets['name']==j].iloc[0]) + ' in ' + str(df_sets['set'][df_sets['name']==j].iloc[0]) + ')'
-#         else:
-#             list_expression_conect[-1] = list_expression_conect[-1] + ' model.P_' + j + '_' + df_conect_to['from'].iloc[i] +'[t]'
-#     list_expressions.append(list_expression_conect)
-
-# print(list_expressions)
-# print('\n')
-# print(list_sets)
-
-
-# #creating constraints from connections
-# class myClass:
-#     pass
-
-# constraint_number = 1
-# for i in list_expressions:
-#     def dynamic_method(model,t,n,m,expr):
-#         return eval(expr, globals(), locals())
-
-#     method_name = 'Constraint_' + str(constraint_number)
-
-#     def method_wrapper(self, model, t, n,m,expr=i):
-#         return dynamic_method(model,t,n,m,expr)
-
-#     setattr(myClass, method_name, method_wrapper)
-
-#     my_obj = myClass()
-
-#     setattr(model, 'constraint_conect' + str(constraint_number), pyo.Constraint(model.HOURS,model.PV,
-#                                                                                 model.BAT,rule=getattr(my_obj, method_name)))
-#     constraint_number = constraint_number + 1
-
-# class MyClass:
-#     pass
-
-# expression = 'model.P_to_demand[t] == model.P_pv_demand[t,n] + model.P_bat_demand[t] + model.P_net_demand[t]'
-
-# def demand_rule(model,t,n,m):
-#     return model.P_to_demand[t] == model.P_pv_demand[t,n] + model.P_bat_demand[t] + model.P_net_demand[t]
-
-'--------------------------------------------------------------------------------------------------------------'
-
-# import inspect
-
-# class MyClass:
-#     pass
-
-# expression = 'model.P_to_demand[t] == model.P_pv_demand[t,n] + model.P_bat_demand[t] + model.P_net_demand[t]'
-
-# def demand_rule(self, model, t, n):
-#     return eval(expression)
-
-# setattr(MyClass, 'demand_rule', demand_rule)
-
-
-# my_object = MyClass()
-
-# methods = dir(my_object)
-
-# for method_name in methods:
-#     # Check if the item is a method
-#     if callable(getattr(my_object, method_name)):
-#         method = getattr(my_object, method_name)
-#         if method.__name__ == 'demand_rule':
-#             method_source = inspect.getsource(method)
-#             print(f"\n{method_name}:")
-#             print(method_source)
-
-
+import inspect
+import pyomo.environ as pyo
 import pandas as pd
 
-# Create a sample DataFrame
-data = {'A': [0, 0, 0, 0, 0],
-        'B': [0, 2, 0, 0, 0],
-        'C': [0, 1, 0, 0, 0]}
-df = pd.DataFrame(data)
+#-------------------------------------------------------------------------------------------------------------
+#region creating string for constraints
 
-# Specify the line number to check (e.g., line 2)
-line_number = 7
+class MyClass:
+    def method_1(self, model, t):
+        return model.P_pv[t] == model.P_pv_bat[t] + model.P_pv_bat[t]
 
-# Check if the specified line has any component different from zero
-line_has_non_zero = (df.loc[line_number] != 0).any()
+myObj = MyClass()
 
-print(line_has_non_zero)
+# Get the original method
+original_method = myObj.method_1
+
+# print(original_method)
+
+# Get the source code of the method
+source_code = inspect.getsource(original_method)
+
+print(source_code)
+
+# Replace the parameter name
+modified_source_code = source_code.replace("P_pv", "P_pv1")
+
+print(modified_source_code)
+
+modified_source_code = 'def method_1(self, model, t): \n return model.demand[t] == model.quant_x[t] + model.quant_y[t]'
+
+# Define the new class with the modified method
+class NewClass:
+    exec(modified_source_code)
+
+# Create an instance of the new class
+newObj = NewClass()
+
+print(inspect.getsource(NewClass))
+
+#endregion
+#-------------------------------------------------------------------------------------------------------------
+#region creating model
+
+model = pyo.AbstractModel()
+
+model.HOURS = pyo.Set()
+
+model.demand = pyo.Param(model.HOURS)
+model.cost_x = pyo.Param(model.HOURS)
+model.cost_y = pyo.Param(model.HOURS)
+
+model.quant_x = pyo.Var(model.HOURS, within=pyo.NonNegativeReals)
+model.quant_y = pyo.Var(model.HOURS, within=pyo.NonNegativeReals)
+model.quant_z = pyo.Var(model.HOURS, within=pyo.NonNegativeReals)
+
+#endregion
+#-------------------------------------------------------------------------------------------------------------
+#region creating constriant
+
+method = getattr(NewClass,'method_1')
+name = 'constraint_test'
+model.add_component(name, pyo.Constraint(model.HOURS, rule = method))
+
+def objective_rule(model,t):
+    return sum(model.quant_x[t] * model.cost_x + model.quant_y[t] * model.cost_y[t] for t in model.HOURS)
+model.objectiveRule =  pyo.Objective(rule=objective_rule,sense = pyo.minimize)
+
+#endregion
+#-------------------------------------------------------------------------------------------------------------
+#region executing the model
+
+path_input = './input/'
+path_output = './output/'
+name_file = 'df_input_test.xlsx'
+
+df_input_series = pd.read_excel(path_input + name_file, sheet_name = 'series')
+
+# reading data
+data = pyo.DataPortal()
+data['HOURS'] = df_input_series['HOURS'].tolist()
+data['cost_x'] = df_input_series.set_index('HOURS')['cost_x'].to_dict()
+data['cost_y'] = df_input_series.set_index('HOURS')['cost_y'].to_dict()
+data['demand'] = df_input_series.set_index('HOURS')['demand'].to_dict()
+
+# generating instance
+instance = model.create_instance(data)
+
+print("Constraint Expressions:")
+for constraint in instance.component_objects(pyo.Constraint):
+    for index in constraint:
+        print(f"{constraint}[{index}]: {constraint[index].body}")
+
+# solving the model
+optimizer = pyo.SolverFactory('cplex')
+results = optimizer.solve(instance)
+
+# # Displaying the results
+# instance.pprint()
+instance.display()
+
+#endregion
